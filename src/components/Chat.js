4a878f6f67428c068a535910818376ba61627663
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2017-08-23 06:58:39 -0500

    Chat Functionality working

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,35 +1,47 @@
-import React from "react";
-import GiftedChat from "react-native-gifted-chat";
-import {
-  View,
-  Text
-} from 'react-native';
+import React from 'react';
 
-class Chat extends React.Component{
+import { GiftedChat } from 'react-native-gifted-chat';
+import Backend from '../Backend';
 
-  state={
-    messages:[]
+export default class Chat extends React.Component {
+  state = {
+    messages: [],
   };
+  componentWillMount() {
 
-  render(){
-
-    return(
+  }
+  render() {
+    return (
       <GiftedChat
         messages={this.state.messages}
-        onSend={(messages) =>
-          //
-        }
+        onSend={(message) => {
+          Backend.sendMessage(message);
+        }}
         user={{
-          _id: 1,
+          _id: Backend.getUid(),
+          name: this.props.name,
         }}
       />
     );
   }
+  componentDidMount() {
+    Backend.loadMessages((message) => {
+      this.setState((previousState) => {
+        return {
+          messages: GiftedChat.append(previousState.messages, message),
+        };
+      });
+    });
+  }
+  componentWillUnmount() {
+    Backend.closeChat();
+  }
 }
 
+Chat.defaultProps = {
+  name: 'John Smith',
+};
 
 Chat.propTypes = {
   name: React.PropTypes.string,
 };
-
-export default Chat;
